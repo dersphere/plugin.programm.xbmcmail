e6bf413450f03adf3f58c266a2adbf890ec12636
dersphere <sphere@dersphere.de>
2013-07-05 12:31:37 +0200

    cosmetic refactor in client

--- resources/lib/client.py
+++ resources/lib/client.py
@@ -77,103 +77,6 @@ class XBMCMailClient(object):
         self.host = host
         self.log('connected.')
 
-    def __parse_list_response(self, line):
-        flags, delimiter, name = self.re_list_response.match(line).groups()
-        name = name.strip('"')
-        return flags, delimiter, name
-
-    def __parse_fetch_response(self, line):
-        email_id, flags_str = self.re_fetch_response.match(line).groups()
-        return email_id, flags_str
-
-    def __parse_status_response(self, line):
-        try:
-            total, unseen = self.re_status_response.match(line).groups()
-        except AttributeError:
-            return 0, 0
-        return total, unseen
-
-    def __decode_header(self, line):
-        if line:
-            return decode_header(line)[0][0]
-        return 'FIXME'
-
-    def __decode_modified_utf7(self, s):
-
-        def modified_deutf7(s):
-            s_utf7 = '+' + s.replace(',', '/') + '-'
-            return s_utf7.encode('latin-1').decode('utf-7')
-
-        r = []
-        _in = []
-        for c in s.decode('latin-1'):
-            if c == '&' and not _in:
-                _in.append('&')
-            elif c == '-' and _in:
-                if len(_in) == 1:
-                    r.append('&')
-                else:
-                    r.append(modified_deutf7(''.join(_in[1:])))
-                _in = []
-            elif _in:
-                _in.append(c)
-            else:
-                r.append(c)
-        if _in:
-            r.append(modified_deutf7(''.join(_in[1:])))
-
-        return ''.join(r)
-
-    def _list_mailboxes(self):
-        self.log('list')
-        ret, data = self.connection.list()
-        self.log(ret)
-        return (self.__parse_list_response(line) for line in data)
-
-    def _select_mailbox(self, mailbox):
-        self.log('select %s' % mailbox)
-        ret, data = self.connection.select(mailbox)
-        self.log(ret)
-        self.selected_mailbox = mailbox
-        return int(data[0])
-
-    def _get_email_ids(self, mailbox=None):
-        if mailbox and mailbox != self.selected_mailbox:
-            self._select_mailbox(mailbox)
-        if not self.selected_mailbox:
-            raise ValueError('No Mailbox selected')
-        self.log('search %s' % DEFAULT_SEARCH_CRITERIA)
-        ret, data = self.connection.search(None, DEFAULT_SEARCH_CRITERIA)
-        self.log(ret)
-        email_ids = data[0].split()
-        email_ids.reverse()
-        return email_ids
-
-    def _fetch_emails_by_ids(self, email_ids, mailbox=None):
-        if mailbox and mailbox != self.selected_mailbox:
-            self._select_mailbox(mailbox)
-        if not self.selected_mailbox:
-            raise ValueError('No Mailbox selected')
-        if isinstance(email_ids, (list, tuple)):
-            email_ids = ','.join(email_ids)
-        self.log('fetch %s' % email_ids)
-        ret, data = self.connection.fetch(email_ids, DEFAULT_FETCH_PARTS)
-        self.log(ret)
-        data = (d for d in data if isinstance(d, tuple))
-        parser = HeaderParser()
-        data = (
-            (self.__parse_fetch_response(status), parser.parsestr(header))
-            for status, header in data
-        )
-        return data
-
-    def _get_mailbox_status(self, mailbox):
-        self.log('status %s' % mailbox)
-        ret, data = self.connection.status(mailbox, DEFAULT_MAILBOX_STATS)
-        self.log(ret)
-        total, unseen = self.__parse_status_response(data[0])
-        return total, unseen
-
     def get_mailboxes(self, fetch_status=True):
         mailboxes = [{
             'name': self.__decode_modified_utf7(name),
@@ -268,6 +171,103 @@ class XBMCMailClient(object):
                 self.log('Mailbox deselected.')
             self.connection.logout()
             self.log('Logged out.')
+
+    def _list_mailboxes(self):
+        self.log('list')
+        ret, data = self.connection.list()
+        self.log(ret)
+        return (self.__parse_list_response(line) for line in data)
+
+    def _select_mailbox(self, mailbox):
+        self.log('select %s' % mailbox)
+        ret, data = self.connection.select(mailbox)
+        self.log(ret)
+        self.selected_mailbox = mailbox
+        return int(data[0])
+
+    def _get_email_ids(self, mailbox=None):
+        if mailbox and mailbox != self.selected_mailbox:
+            self._select_mailbox(mailbox)
+        if not self.selected_mailbox:
+            raise ValueError('No Mailbox selected')
+        self.log('search %s' % DEFAULT_SEARCH_CRITERIA)
+        ret, data = self.connection.search(None, DEFAULT_SEARCH_CRITERIA)
+        self.log(ret)
+        email_ids = data[0].split()
+        email_ids.reverse()
+        return email_ids
+
+    def _fetch_emails_by_ids(self, email_ids, mailbox=None):
+        if mailbox and mailbox != self.selected_mailbox:
+            self._select_mailbox(mailbox)
+        if not self.selected_mailbox:
+            raise ValueError('No Mailbox selected')
+        if isinstance(email_ids, (list, tuple)):
+            email_ids = ','.join(email_ids)
+        self.log('fetch %s' % email_ids)
+        ret, data = self.connection.fetch(email_ids, DEFAULT_FETCH_PARTS)
+        self.log(ret)
+        data = (d for d in data if isinstance(d, tuple))
+        parser = HeaderParser()
+        data = (
+            (self.__parse_fetch_response(status), parser.parsestr(header))
+            for status, header in data
+        )
+        return data
+
+    def _get_mailbox_status(self, mailbox):
+        self.log('status %s' % mailbox)
+        ret, data = self.connection.status(mailbox, DEFAULT_MAILBOX_STATS)
+        self.log(ret)
+        total, unseen = self.__parse_status_response(data[0])
+        return total, unseen
+
+    def __parse_list_response(self, line):
+        flags, delimiter, name = self.re_list_response.match(line).groups()
+        name = name.strip('"')
+        return flags, delimiter, name
+
+    def __parse_fetch_response(self, line):
+        email_id, flags_str = self.re_fetch_response.match(line).groups()
+        return email_id, flags_str
+
+    def __parse_status_response(self, line):
+        try:
+            total, unseen = self.re_status_response.match(line).groups()
+        except AttributeError:
+            return 0, 0
+        return total, unseen
+
+    def __decode_header(self, line):
+        if line:
+            return decode_header(line)[0][0]
+        return 'FIXME'
+
+    def __decode_modified_utf7(self, s):
+
+        def modified_deutf7(s):
+            s_utf7 = '+' + s.replace(',', '/') + '-'
+            return s_utf7.encode('latin-1').decode('utf-7')
+
+        r = []
+        _in = []
+        for c in s.decode('latin-1'):
+            if c == '&' and not _in:
+                _in.append('&')
+            elif c == '-' and _in:
+                if len(_in) == 1:
+                    r.append('&')
+                else:
+                    r.append(modified_deutf7(''.join(_in[1:])))
+                _in = []
+            elif _in:
+                _in.append(c)
+            else:
+                r.append(c)
+        if _in:
+            r.append(modified_deutf7(''.join(_in[1:])))
+
+        return ''.join(r)
 
     def __del__(self):
         self.logout()
